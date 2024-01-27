4a7b1d01a1e4d6a2a2f4dec200b2097706e739ad
FragAverage <jamie@totaldrift.com>
2024-01-27 11:27:17 +0000

    More Script and CICD work

--- startup.sh
+++ startup.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
 install_or_update() {
-  /home/steam/steamcmd/steamcmd.sh +force_install_dir /home/steam/palworld +login anonymous +app_update 2394010 validate +exit
+  /home/steam/steamcmd/steamcmd.sh +force_install_dir /home/steam/palworld +login anonymous +app_update 2394010 validate +quit
 }
 
 start_server() {
   install_or_update 
 
-  echo "Running on port: $PORT"
+  ARGS=" EpicApp=PalServer -useperfthreads -NoAsyncLoadThread -UseMultitheadForDS"
+
+
+
+  echo "Starting on port: $PORT"
+  
+  cd /home/steam/palworld
+  ./PalServer.sh "$ARGS"
+
 }
 
-start_server
+
+term_handler() {
+	kill -SIGTERM $(pidof PalServer-Linux-Test)
+	tail --pid=$(pidof PalServer-Linux-Test) -f 2>/dev/null
+	exit 143;
+}
+
+trap 'kill ${!}; term_handler' SIGTERM
+
+start_server &
+killpid="$!"
+while true
+do
+  wait $killpid
+  exit 0;
+done
